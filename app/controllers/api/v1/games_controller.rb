612313d654851e9204bcded9d503297d698a0a95
harirordeveloper@gmail.com <harirordeveloper@gmail.com>
2021-02-12 00:10:39 +0530

    indentation fix

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,31 +1,31 @@
 module Api
   module V1
     class GamesController < BaseController
-    	before_action :fetch_game, only: [:update]
+      before_action :fetch_game, only: [:update]
 
-    	def create
-    		@game = Game.create(game_params)
+      def create
+        @game = Game.create(game_params)
 
-    		render json: { gameId: @game.id, players: @game.players.as_json(only: [:id, :name]) }
-			end
+        render json: { gameId: @game.id, players: @game.players.as_json(only: [:id, :name]) }
+      end
 
-			def update
-				@game.update(game_update_params)
-			end
+      def update
+        @game.update(game_update_params)
+      end
 
-			private
+      private
 
-				def game_params
-					params.permit(:game_over, :winner_id, :gamer_squares, :next_turn, :last_move, gamer_squares: [], squares: [], plays_attributes: :player_id)
-				end
+        def game_params
+          params.permit(:game_over, :winner_id, :gamer_squares, :next_turn, :last_move, gamer_squares: [], squares: [], plays_attributes: :player_id)
+        end
 
-				def game_update_params
-					params.permit(:game_over, :winner_id, :gamer_squares, :next_turn, :last_move, gamer_squares: [], squares: [])
-				end
+        def game_update_params
+          params.permit(:game_over, :winner_id, :gamer_squares, :next_turn, :last_move, gamer_squares: [], squares: [])
+        end
 
-				def fetch_game
-					@game ||= Game.find_by(id: params[:id])
-				end
+        def fetch_game
+          @game ||= Game.find_by(id: params[:id])
+        end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,19 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  	def configure_permitted_parameters
-	    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-	    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-	  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
   
   private
      
 
     def after_sign_out_path_for(resource_or_scope)
-	    root_path
-	  end
+      root_path
+    end
 
-	  def after_sign_in_path_for(resource_or_scope)
-	    authenticated_root_path
-	  end
+    def after_sign_in_path_for(resource_or_scope)
+      authenticated_root_path
+    end
 end
